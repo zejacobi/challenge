fb1c12f58daf341b1886962dc9cd524c79517307
zejacobi <zejacobi@gmail.com>
2015-11-03 21:48:20 -0500

    -Finished tests and functions for parsing product info into word lists

--- linkage.py
+++ linkage.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import random
 import math
+import re
 
 random.seed(1)
 
@@ -14,7 +15,7 @@ def import_json_file(file_path):
     :rtype: list
     """
     with open(file_path) as file_obj:
-        lines = file_obj.read().split('\n')
+        lines = file_obj.read().splitlines()
         return [json.loads(line) for line in lines]
 
 
@@ -31,7 +32,41 @@ def cross_validation_sets(dataset, fraction):
     test = dataset[num_recs:]
     return train, test
 
+
+def parse_products(products_data):
+    """
+    :param list products_data:
+    :rtype: list
+    """
+    product_tags = []
+    expr = re.compile(r'[-_(),\s]+')
+    for entry in products_data:
+        name_tags = list(set(re.split(expr, entry[u'product_name'])))
+        product_dict = {u'name_tags': None,
+                        u'manufacturer_tags': list(set(re.split(r'[-_().,\s]+', entry[u'manufacturer'])))}
+        if u'family' in entry.keys():
+            name_tags.extend(list(set(re.split(expr, entry[u'family']))))
+        if u'model' in entry.keys():
+            name_tags.extend(list(set(re.split(expr, entry[u'model']))))
+        name_tags = list(set(name_tags))
+        product_dict[u'name_tags'] = name_tags
+        product_tags.append(product_dict)
+    return product_tags
+
+
+def parse_listings(listings_data):
+    """
+    :param list products_data:
+    :rtype: list
+    """
+    expr = re.compile(r'[-_(),\s]+')
+    return [{u'name_tags': list(set(re.split(expr, entry[u'title']))),
+             u'manufacturer_tags': list(set(re.split(expr, entry[u'manufacturer'])))}
+            for entry in listings_data]
+
+
 # Analysis Functions #
+
 
 ### Main ###
 if __name__ == '__main__':
@@ -43,3 +78,5 @@ if __name__ == '__main__':
 
     listings = args.listings
     products = args.products
+
+    print parse_listings(import_json_file(listings))[:100]
--- tests.py
+++ tests.py
@@ -19,3 +19,23 @@ class TestImport(TestCase):
 
     def test_cross_validation_sets(self):
         self.assertEqual(linkage.cross_validation_sets(self.json, 0.75), self.sets)
+
+
+class TestParsing(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.product_json = [{u'product_name': u'Canon_PowerShot_S20', u'manufacturer': u'Canon',
+                            u'model': u'S20', u'family': u'PowerShot',
+                            u'announced-date': u'2000-01-05T19:00:00.000-05:00'}]
+        cls.listing_json = [{u'title': u'Canon PowerShot S20 3.2MP Digital Camera w/ 2x Optical Zoom',
+                            u'manufacturer': u'Canon USA', u'currency': u'USD', u'price': 59.50}]
+        cls.product_tags = [{u'name_tags': [u'PowerShot', u'Canon', u'S20'], u'manufacturer_tags': [u'Canon']}]
+        cls.listing_tags = [{u'name_tags': [u'Optical', u'w/', u'2x', u'PowerShot', u'Canon', u'3.2MP', u'Zoom', u'S20',
+                                            u'Camera', u'Digital'],
+                             u'manufacturer_tags': [u'Canon', u'USA']}]
+        
+    def test_parse_products(self):
+        self.assertEqual(linkage.parse_products(self.product_json), self.product_tags)
+
+    def test_parse_listings(self):
+        self.assertEqual(linkage.parse_listings(self.listing_json), self.listing_tags)
